92f6fd0582558c7e67777518b950283581189da9
jikor1st <jikor1st@naver.com>
2024-08-30 17:19:39 +0900

    test

---
+++ index.js
@@ -0,0 +1,28 @@
+const webdriver = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+
+const run = async () => {
+  const url = "https://www.google.com";
+
+  let driver;
+  try {
+    const opts = new chrome.Options();
+
+    opts.addArguments("--headless");
+    opts.addArguments("--no-sandbox");
+    opts.addArguments("--single-process");
+    opts.addArguments("--disable-dev-shm-usage");
+
+    driver = await new webdriver.Builder()
+      .forBrowser("chrome")
+      .setChromeOptions(opts)
+      .build();
+
+    await driver.get(url);
+    console.log("driver: ", driver);
+  } catch (error) {
+    console.log("error: ", error);
+  }
+};
+
+run();
